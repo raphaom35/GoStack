e77ab76eb0d151c0d7f0dd2624dc5c3404a36b07
Raphael Martins <rapha35om.rm@gmail.com>
2020-02-24 11:30:47 -0300

    Curso 20_02

--- modulo02/src/app/controllers/UserControle.js
+++ modulo02/src/app/controllers/UserControle.js
@@ -1,13 +1,60 @@
+import * as Yup from 'yup';
 import User from '../models/user';
 
 class UserControle {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Validation falis' });
+    }
     const userExist = await User.findOne({ where: { email: req.body.email } });
     if (userExist) {
       return res.status(400).json({ erro: 'User already exits' });
     }
     const { id, name, email, provider } = await User.create(req.body);
 
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.Ref('password')]) : field
+      ),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    console.log(user);
+    if (email != user.email) {
+      const userExist = await User.findOne({ where: { email } });
+      if (userExist) {
+        return res.status(400).json({ erro: 'User already exits' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ erro: 'Password dos no match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
     return res.json({ id, name, email, provider });
   }
 }
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import UserControle from './app/controllers/UserControle';
 import Session from './app/controllers/SessionControle';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserControle.store);
 routes.post('/sessions', Session.store);
+routes.use(authMiddleware);
+routes.put('/users', UserControle.update);
 
 export default routes;
